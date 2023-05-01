cb4d75f7d872aee1363e5f3e2bfdd985d49d91bc
pc <monu sharma>
2023-05-01 20:18:37 +0530

    Bootstrap-Add

--- stridentWeb/src/app/app.module.ts
+++ stridentWeb/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
