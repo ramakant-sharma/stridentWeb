1c8b00d36ccafd8d9c0ca77713fcd5ce552c0b3f
pc <monu sharma>
2023-05-06 12:06:25 +0530

    footer ui

---
+++ stridentWeb/src/app/home/components/our-blogs/our-blogs.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OurBlogsComponent } from './our-blogs.component';
+
+describe('OurBlogsComponent', () => {
+  let component: OurBlogsComponent;
+  let fixture: ComponentFixture<OurBlogsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OurBlogsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OurBlogsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- stridentWeb/src/app/home/home.module.ts
+++ stridentWeb/src/app/home/home.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { OurBlogsComponent } from './components/our-blogs/our-blogs.component';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    OurBlogsComponent
   ],
   imports: [
     CommonModule,
